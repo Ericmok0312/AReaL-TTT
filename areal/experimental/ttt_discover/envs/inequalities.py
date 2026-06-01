74b6870fb20b2f6f279fb1eeaad16efc44b28613
EricMok0312 <weemokkachun@gmail.com>
2026-06-02 00:32:17 +0800

    restore prompt

--- areal/experimental/ttt_discover/envs/inequalities.py
+++ areal/experimental/ttt_discover/envs/inequalities.py
@@ -201,7 +201,7 @@ Your task is to generate the sequence of non-negative heights of a step function
 
 {AC1_LITERATURE}
 
-Your task is to write a search function that searches for the best sequence of coefficients. Your function will have {budget_s} seconds to run, and after that it has to have returned the best sequence it found. If after {budget_s} seconds it has not returned anything, it will be terminated with negative infinity points. All numbers in your sequence have to be positive or zero. Larger sequences with 1000s of items often have better attack surface, but too large sequences with 100s of thousands of items may be too slow to search. **Important: try multiple sequence lengths (e.g., 500, 1000, 2000, 5000, 10000) and compare their scores — the optimal length is a critical hyperparameter.**
+Your task is to write a search function that searches for the best sequence of coefficients. Your function will have {budget_s} seconds to run, and after that it has to have returned the best sequence it found. If after {budget_s} seconds it has not returned anything, it will be terminated with negative infinity points. All numbers in your sequence have to be positive or zero. Larger sequences with 1000s of items often have better attack surface, but too large sequences with 100s of thousands of items may be too slow to search.
 
 You may code up any search method you want, and you are allowed to call the evaluate_sequence() function as many times as you want. You have access to it, you don't need to code up the evaluate_sequence() function.
 
